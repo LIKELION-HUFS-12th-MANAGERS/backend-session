885565402f977d33b6d9f54bec4d74b3bfa7b55a
Chaeyoung714 <202102760@hufs.ac.kr>
2024-03-11 17:24:40 +0900

    배포 초기세팅

--- modelproject/modelproject/settings.py
+++ modelproject/modelproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$_8+aduj20j8ok)e^un_rlq+^ci=5kbde^n!#!ew8%$iv-v&$y'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,3 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+# 배포 설정
+STATIC_ROOT = BASE_DIR / 'static'
