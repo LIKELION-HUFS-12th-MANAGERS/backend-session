aeb071a44d79df3752485982ac58cb4ea0f6fc48
Chaeyoung714 <202102760@hufs.ac.kr>
2024-03-11 17:32:52 +0900

    배포 세팅 2

--- modelproject/modelproject/settings.py
+++ modelproject/modelproject/settings.py
@@ -76,13 +76,26 @@ WSGI_APPLICATION = 'modelproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': config('test'),
+        'USER': config('root'),
+        'PASSWORD': config('likelion12'),
+        'HOST': config('django-insecure-$_8+aduj20j8ok)e^un_rlq+^ci=5kbde^n!#!ew8%$iv-v&$y'),
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
